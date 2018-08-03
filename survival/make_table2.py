3f23d90bc58e95918236e17ed373aa2bd92f2437
Shannon McCurdy <smccurdy@berkeley.edu>
2018-08-03 16:36:32 -0700

    update table 2

--- survival/make_table2.py
+++ survival/make_table2.py
@@ -46,6 +46,7 @@ def make_table2(root_directory, gold_standard):
 				with open(root_directory + 'project_' +disease_type +'/analysis/final_fit_results_cox/model_selection_output_coxfinal.txt', 'r') as g:
 					with open(root_directory + 'project_' +disease_type +'/analysis/final_fit_results_pca_cox/model_selection_output_pca_coxfinal.txt', 'r') as h:
 						with open(root_directory + 'project_' +disease_type +'/analysis/final_fit_results_cox_gs/model_selection_output_coxfinal.txt', 'r') as i:
+							print('hello')
 							Table2.loc[disease_type, "Test Set c-index"] = [np.round(eval(f.readline())[-1][0], 2), np.round(eval(g.readline())[-1][0], 2), np.round(eval(h.readline())[-1][0], 2), np.round(eval(i.readline())[-1][0], 2)]
 		else:
 			Table2.loc[disease_type, "Disease"] = [mod, 4+ mod_cox, 7+ mod_pca_cox]
