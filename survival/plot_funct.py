948ddd277dc6aba74110e05894acd2872962e8b0
Shannon McCurdy <smccurdy@berkeley.edu>
2019-04-06 18:01:41 -0700

    more font size adjustments and rotations

--- survival/plot_funct.py
+++ survival/plot_funct.py
@@ -73,7 +73,7 @@ def prettyplot(n_cv, i, survivalloc, analysis_directory, disease_type):
 	ax.tick_params(axis='both', labelsize=24)
 	ax.set_xlabel(r'$E[z_1| x]$', fontsize=24)
 	ax.set_ylabel(r'$E[z_2| x]$', fontsize=24)
-	locs, labels= plt.xticks()
+	labels = [item.get_text() for item in ax.get_xticklabels()]
 	ax.set_xticklabels(labels, rotation='vertical')
 	#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 	# ax.set_title('Volume and percent change')
@@ -111,7 +111,7 @@ def prettyplot(n_cv, i, survivalloc, analysis_directory, disease_type):
 	axv.tick_params(axis='both', labelsize=24)
 	axv.set_xlabel(r'$E[z_1| x]$', fontsize=24)
 	axv.set_ylabel(r'$E[z_2| x]$', fontsize=24)
-	locs, labels= plt.xticks()
+	labels = [item.get_text() for item in axv.get_xticklabels()]
 	axv.set_xticklabels(labels, rotation='vertical')
 	#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 	# ax.set_title('Volume and percent change')
@@ -156,7 +156,7 @@ def prettyplot2(n_cv, i, survivalloc, colorvector, colorname, analysis_directory
 	ax.tick_params(axis='both', labelsize=24)
 	ax.set_xlabel(r'$E[z_1| x]$', fontsize=24)
 	ax.set_xlabel(r'$E[z_2| x]$', fontsize=24)
-	locs, labels= plt.xticks()
+	labels = [item.get_text() for item in ax.get_xticklabels()]
 	ax.set_xticklabels(labels, rotation='vertical')
 	#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 	# ax.set_title('Volume and percent change')
@@ -215,7 +215,7 @@ def prettyplot3(n_cv, i, survivalloc, colorcat, colorname, analysis_directory, d
 	ax.tick_params(axis='both', labelsize=24)
 	ax.set_xlabel(r'$E[z_1| x]$', fontsize=24)
 	ax.set_ylabel(r'$E[z_2| x]$', fontsize=24)
-	locs, labels= plt.xticks()
+	labels = [item.get_text() for item in ax.get_xticklabels()]
 	ax.set_xticklabels(labels, rotation='vertical')
 	#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 	# ax.set_title('Volume and percent change')
@@ -276,7 +276,7 @@ def prettyplot3_pca(n_cv, i, survivalloc, colorcat, colorname, analysis_director
 	ax.tick_params(axis='both', labelsize=24)
 	ax.set_xlabel(r'$U_1$', fontsize=24)
 	ax.set_ylabel(r'$U_2$', fontsize=24)
-	locs, labels= plt.xticks()
+	labels = [item.get_text() for item in ax.get_xticklabels()]
 	ax.set_xticklabels(labels, rotation='vertical')
 	#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 	# ax.set_title('Volume and percent change')
@@ -324,7 +324,7 @@ def prettyplot_final(survivalloc, analysis_directory, disease_type):
 				ax.tick_params(axis='both', labelsize=24)
 				ax.set_xlabel(r'$E[z_%d| x]$' % (z2 +1), fontsize=24)
 				ax.set_ylabel(r'$E[z_%d| x]$' %(z1 +1), fontsize=24)
-				locs, labels= plt.xticks()
+				labels = [item.get_text() for item in ax.get_xticklabels()]
 				ax.set_xticklabels(labels, rotation='vertical')
 				#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 				# ax.set_title('Volume and percent change')
@@ -359,7 +359,7 @@ def prettyplot_final(survivalloc, analysis_directory, disease_type):
 				axv.tick_params(axis='both', labelsize=24)
 				axv.set_xlabel(r'$E[z_%d| x]$' %(z2 +1), fontsize=24)
 				axv.set_ylabel(r'$E[z_%d| x]$' %(z1 +1), fontsize=24)
-				locs, labels= plt.xticks()
+				labels = [item.get_text() for item in axv.get_xticklabels()]
 				axv.set_xticklabels(labels, rotation='vertical')
 				#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 				# ax.set_title('Volume and percent change')
@@ -428,7 +428,7 @@ def prettyplot3_final(survivalloc, colorcat, colorname, analysis_directory, dise
 				ax.tick_params(axis='both', labelsize=24)
 				ax.set_xlabel(r'$E[z_%d| x]$'%(z2 +1), fontsize=24)
 				ax.set_ylabel(r'$E[z_%d| x]$'%(z1 +1), fontsize=24)
-				locs, labels= plt.xticks()
+				labels = [item.get_text() for item in ax.get_xticklabels()]
 				ax.set_xticklabels(labels, rotation='vertical')
 				#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 				# ax.set_title('Volume and percent change')
@@ -475,7 +475,7 @@ def prettyplot3_final(survivalloc, colorcat, colorname, analysis_directory, dise
 				ax_v.tick_params(axis='both', labelsize=24)
 				ax_v.set_xlabel(r'$E[z_%d| x]$'%(z2 +1), fontsize=24)
 				ax_v.set_ylabel(r'$E[z_%d| x]$'%(z1 +1), fontsize=24)
-				locs, labels= plt.xticks()
+				labels = [item.get_text() for item in ax_v.get_xticklabels()]
 				ax_v.set_xticklabels(labels, rotation='vertical')
 				#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 				# ax.set_title('Volume and percent change')
@@ -535,7 +535,7 @@ def prettyplot3_final_sim(colorcat, colorname, root_directory, project_name, dis
 				ax_v.tick_params(axis='both', labelsize=24)
 				ax_v.set_xlabel(r'$ z_%d$'%(z2 +1), fontsize=24)
 				ax_v.set_ylabel(r'$ z_%d$'%(z1 +1), fontsize=24)
-				locs, labels= plt.xticks()
+				labels = [item.get_text() for item in ax_v.get_xticklabels()]
 				ax_v.set_xticklabels(labels, rotation='vertical')
 				#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 				# ax.set_title('Volume and percent change')
@@ -603,7 +603,7 @@ def prettyplot3_final_sim(colorcat, colorname, root_directory, project_name, dis
 				# ax.set_ylabel(r'$Q E[z_%d| x]$'%(z1 +1), fontsize=20)
 				ax.set_xlabel(r'$E[z_%d| x]$'%(z2 +1), fontsize=24)
 				ax.set_ylabel(r'$E[z_%d| x]$'%(z1 +1), fontsize=24)
-				locs, labels= plt.xticks()
+				labels = [item.get_text() for item in ax.get_xticklabels()]
 				ax.set_xticklabels(labels, rotation='vertical')
 				#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 				# ax.set_title('Volume and percent change')
@@ -647,7 +647,7 @@ def prettyplot3_final_sim(colorcat, colorname, root_directory, project_name, dis
 				ax_v.tick_params(axis='both', labelsize=24)
 				ax_v.set_xlabel(r'$E[z_%d| x]$'%(z2 +1), fontsize=24)
 				ax_v.set_ylabel(r'$E[z_%d| x]$'%(z1 +1), fontsize=24)
-				locs, labels= plt.xticks()
+				labels = [item.get_text() for item in ax_v.get_xticklabels()]
 				ax_v.set_xticklabels(labels, rotation='vertical')
 				# ax_v.set_xlabel(r'$Q E[z_%d| x]$'%(z2 +1), fontsize=20)
 				# ax_v.set_ylabel(r'$ Q E[z_%d| x]$'%(z1 +1), fontsize=20)
