0a2786b876ffdc8b74b429d27f7a9e4e3c0300a3
Shannon McCurdy <smccurdy@berkeley.edu>
2019-04-06 18:05:17 -0700

    more font size adjustments and rotations

--- survival/plot_funct.py
+++ survival/plot_funct.py
@@ -73,8 +73,8 @@ def prettyplot(n_cv, i, survivalloc, analysis_directory, disease_type):
 	ax.tick_params(axis='both', labelsize=24)
 	ax.set_xlabel(r'$E[z_1| x]$', fontsize=24)
 	ax.set_ylabel(r'$E[z_2| x]$', fontsize=24)
-	print(ax.get_xticklabels())
 	labels = [item.get_text() for item in ax.get_xticklabels()]
+	print(labels)
 	ax.set_xticklabels(labels, rotation='vertical')
 	#ax.arrow(0, 0, paramt[0][1][0][0][0], paramt[0][1][0][0][1], head_width=.3, head_length=.3, fc='k', ec='k')
 	# ax.set_title('Volume and percent change')
